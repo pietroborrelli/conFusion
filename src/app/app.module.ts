da41738cb36ec479c5f23911d3adb1d1340a3c0e
Pietro Borrelli <pietro.borrelli@outlook.com>
2019-09-12 08:29:38 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//NgModule is a decorator is a function that modifies Javascript classes
+//we'll use more decorators
+//inside there are metadatas 
 @NgModule({
+  //declares the view classes that belong to this particular module
+  //a view class = components, directives, and pipes
   declarations: [
     AppComponent
   ],
+  //all these modules need to be imported to be used with this app module. So this app module now depends upon these other modules.
+  //So when you import the modules, you are specifying here saying that the app module will make use of this module
   imports: [
     BrowserModule
   ],
   providers: [],
+  //bootstrap the application with the AppComponent (=root component)
   bootstrap: [AppComponent]
 })
+
+//typescript has support for classes
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+//from this libraries i'm importing these modules
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -8,6 +9,8 @@ if (environment.production) {
   enableProdMode();
 }
 
+//app module is the root module, from which the application bootstraps
+//see src/app module
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
 
