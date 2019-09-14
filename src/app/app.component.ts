4478520e85f6f0f34efcf57907577ae58b16c016
Pietro Borrelli <pietro.borrelli@outlook.com>
2019-09-14 18:13:32 +0200

    Configuring Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+//decorator
 @Component({
+  //definition of the component (app-root is called from index.hmtl)
   selector: 'app-root',
+  //define the view
   templateUrl: './app.component.html',
+  //CSS styling 
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
 
 //NgModule is a decorator is a function that modifies Javascript classes
 //we'll use more decorators
@@ -15,8 +21,12 @@ import { AppComponent } from './app.component';
   //all these modules need to be imported to be used with this app module. So this app module now depends upon these other modules.
   //So when you import the modules, you are specifying here saying that the app module will make use of this module
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule
   ],
+  //providera specify all the services that this particular module will make use of.
   providers: [],
   //bootstrap the application with the AppComponent (=root component)
   bootstrap: [AppComponent]
