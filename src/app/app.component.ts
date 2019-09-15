7bf8f3ff42390c87c713aa21912c49e4cc4f247e
Pietro Borrelli <pietro.borrelli@outlook.com>
2019-09-15 12:08:09 +0200

    Component part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { Component } from '@angular/core';
 @Component({
   //definition of the component (app-root is called from index.hmtl)
   selector: 'app-root',
-  //define the view
+  //define the view of which the component cares of
   templateUrl: './app.component.html',
   //CSS styling 
   styleUrls: ['./app.component.scss']
 })
+//whenever you want to make any component or a module in portable in another module, then you would always prepend the export to the class here
 export class AppComponent {
+  //title is accessible from the templates 
   title = 'conFusion';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+
+//automaticcaly added when this component is created and can be used in the application
+import { MenuComponent } from './menu/menu.component';
 
 //NgModule is a decorator is a function that modifies Javascript classes
 //we'll use more decorators
@@ -16,7 +20,8 @@ import 'hammerjs';
   //declares the view classes that belong to this particular module
   //a view class = components, directives, and pipes
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   //all these modules need to be imported to be used with this app module. So this app module now depends upon these other modules.
   //So when you import the modules, you are specifying here saying that the app module will make use of this module
@@ -24,7 +29,8 @@ import 'hammerjs';
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
   ],
   //providera specify all the services that this particular module will make use of.
   providers: [],
